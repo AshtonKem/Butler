53e85f6c09444b121e3dc00962e5eb296fb4d294
Ashton Kemerling <ashtonkemerling@mail.com>
2013-03-31 22:09:55 -0500

    Add notice.

---
+++ butler.el
@@ -0,0 +1,34 @@
+;;; butler.el --- Client for Jenkins
+
+;; Copyright © 2012-2013 Ashton Kemerling
+;;
+;; Author: Ashton Kemerling <ashtonkemerling@gmail.com>
+;; URL: http://www.github.comc/AshtonKem/Butler.git
+;; Version: 0.0.1
+;; Keywords: Jenkins, Hudson, CI
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;; This file is not part of GNU Emacs.
+
+;;; Commentary:
+
+;; Provides an interface to connect to the Jenkins CI server
+
+;;; Installation
+
+;; Will eventually be available in elpa or marmalade
+
+
+;;; Code:
